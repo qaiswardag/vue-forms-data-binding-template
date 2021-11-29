c7737aef258ec187ab4fcd6c3f87a25e65a3c09a
qaiswardag <qais.wardag@outlook.com>
2021-11-29 17:26:44 +0100

    modify styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,86 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>Form</h1>
+    <form @submit.prevent="handleSubmit">
+      <label>Email:</label>
+      <input type="email" v-model="email" required>
+
+      <label>Password:</label>
+      <input type="password" v-model="password" required>
+      <div v-if="passwordError" class="error">{{ passwordError }}</div>
+
+      <label>Role:</label>
+      <select v-model="role">
+        <option value="developer">Web Developer</option>
+        <option value="designer">Web Designer</option>
+      </select>
+
+      <label>Skills (press Comma to add):</label>
+      <input type="text" v-model="tempSkill" @keyup.,="addSkill">
+      <!-- <input type="text" v-model="tempSkill" @keyup.alt="addSkill"> -->
+      <div v-for="skill in skills" :key="skill" class="pill">
+        <span @click="deleteSkill(skill)">{{ skill }}</span>
+      </div>
+
+      <div class="terms">
+        <input type="checkbox" v-model="terms" required>
+        <label>Accept terms and conditions</label>
+      </div>
+
+      <div class="submit">
+        <button class="btn btn--top-margin btn--display-block">Create an Account</button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      email: '',
+      password: '',
+      role: 'developer',
+      terms: false,
+      skills: [],
+      tempSkill: '',
+      movies: [],
+      tempMovie: '',
+      passwordError: null,
+    }
+  },
+  methods: {
+    addSkill($event) {
+      if ($event.key === ',' && this.tempSkill) {
+        if (!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill.slice(0, -1))
+        }
+        this.tempSkill = ''
+      }
+    },
+    //
+    deleteSkill(skill) {
+      this.skills = this.skills.filter((item) => {
+        return skill !== item
+      })
+    },
+    handleSubmit() {
+      // validate password
+      this.passwordError =
+        this.password.length > 5
+          ? ''
+          : 'Password must be at least 6 characters long'
+
+      if (!this.passwordError) {
+        // make request to database to save user
+        console.log('email: ', this.email)
+        console.log('password: ', this.password)
+        console.log('role: ', this.role)
+        console.log('skills: ', this.skills)
+        console.log('terms accepted: ', this.terms)
+      }
+    },
+  },
 }
 </script>
+
